e5ac09e2d8fb10fe624ddfd66b162cb4fdafe796
root <aurelle.adjahou@gmail.com>
2022-03-21 08:33:20 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,6 +5,7 @@
  */
 void print_rev(char *s)
 {
+
 int len = 0, index;
 
 while (s[index++])
@@ -14,4 +15,5 @@ for (index = len - 1; index >= 0; index--)
 _putchar(s[index]);
 
 _putchar('\n');
+ 
 }
