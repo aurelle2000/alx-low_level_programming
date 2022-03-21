42521862d9f0dfdfc3ca1eee2a75454c299fe8fc
root <aurelle.adjahou@gmail.com>
2022-03-21 09:44:18 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-	/**
-	 * _atoi - Converts a string to an integer.
-	 * @s: The string to be converted.
-	 *
-	 * Return: The integer value of the converted string.
-	 */
+/**
+* _atoi - Converts a string to an integer.
+* @s: The string to be converted.
+*
+* Return: The integer value of the converted string.
+*/
 int _atoi(char *s)
 {
 int sign = 1;
