0d58c12cc07d3c58599d85f38e765b545907035d
root <aurelle.adjahou@gmail.com>
2022-04-04 21:38:40 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,24 +5,17 @@
  *
  * Return: Nothing
  */
-void print_number(int n)
+void print_number(int n
 {
-	int divisor = 1, i, resp;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		n *= -1;
-	}
-
-	for (i = 0; n / divisor > 9; i++, divisor *= 10)
-		;
-
-	for (; divisor >= 1; n %= divisor, divisor /= 10)
-	{
-		resp = n / divisor;
-		_putchar('0' + resp);
-
-	}
-
+	print_number(98);
+        _putchar('\n');
+	print_number(402);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
+	return (0);
 }
