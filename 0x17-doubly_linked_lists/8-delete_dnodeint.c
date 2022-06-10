7384730be70860f606e2b3b7aff5fdec7b3578cd
root <aurelle.adjahou@gmail.com>
2022-06-09 20:03:55 -0700

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,54 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - that deletes the node at index
- * @head: variable pointer to pointer
- * @index: index to delete
- * Return: 1 if it succeeded, -1 if it failed
+ * delete_dnodeint_at_index - delete node at specific index
+ * @head: head of linked list
+ * @index: index of node to delete
+ * Return: 1 on success, -1 on failure
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int x = 0;
-	dlistint_t *i, *currentNode, *sigNode, *prevNode;
+	dlistint_t *current;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (-1);
-	for (i = *head; i != NULL; i = i->next)
+
+	current = *head;
+
+	if (current == NULL)
+		return (-1);
+
+	while (index != 0)
 	{
-		if (x == index)
-		{
-			currentNode = i;
-			sigNode = currentNode->next;
-			prevNode = currentNode->prev;
-			if (currentNode->prev == NULL && currentNode->next != NULL)
-			{
-				sigNode->prev = NULL;
-				*head = sigNode;
-			}
-			else if (currentNode->next == NULL && currentNode->prev != NULL)
-			{
-				prevNode->next = NULL;
-				*head = prevNode;
-			}
-			else
-			{
-				if (currentNode->next != NULL && currentNode->prev != NULL)
-				{
-					sigNode->prev = prevNode;
-					prevNode->next = sigNode;
-				}
-				else
-				{
-					currentNode = NULL;
-					*head = NULL;
-				}
-			}
-			free(currentNode);
-		}
-		x++;
+		current = current->next;
+		index--;
+		if (current == NULL)
+			return (-1);
 	}
+
+	if (current->prev != NULL)
+		current->prev->next = current->next;
+	else
+	{
+		*head = current->next;
+		if (current->next != NULL)
+			current->next->prev = NULL;
+		free(current);
+	}
+
 	return (1);
 }
