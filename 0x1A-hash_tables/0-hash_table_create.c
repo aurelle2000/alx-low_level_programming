cb6eab070836223c031d439a9df03ade2ca68769
root <aurelle.adjahou@gmail.com>
2022-07-06 18:40:24 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,11 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_create - creates a hash tables
  * @size: the array (hash table) size
  * Return: a  pointer to the newly created hash table
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ptr_to_array;
@@ -24,4 +26,4 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	return (ptr_to_array);
-}
+	
